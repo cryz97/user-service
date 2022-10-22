59cc116d3aa6afeecfa6f7b5392f4ead0c1a657d
Chris Macias <chris.macias.dev@gmail.com>
2022-10-22 17:54:56 -0500

    Initial commit

---
+++ src/test/java/com/improving/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.improving.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
