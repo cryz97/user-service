335bfeddd9cc8539871bbd8cfa8cae79c28e1a95
Chris Macias <chris.macias.dev@gmail.com>
2022-10-24 17:33:25 -0500

    Was implemented Open API 3 for API Documentation and added Swagger UI for integration test

--- src/main/java/com/improving/userservice/controller/UserController.java
+++ src/main/java/com/improving/userservice/controller/UserController.java
@@ -3,6 +3,12 @@ package com.improving.userservice.controller;
 import com.improving.userservice.dto.UserCreationDTO;
 import com.improving.userservice.dto.UserDTO;
 import com.improving.userservice.service.impl.UserServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "User")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -17,22 +24,51 @@ public class UserController {
     @Autowired
     UserServiceImpl userService;
 
+    @Operation(summary = "Get all the Users")
+    @ApiResponse(responseCode = "200", description = "Found all active Users",
+            content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserDTO.class)) })
     @GetMapping("/")
     public ResponseEntity<List<UserDTO>> getAllUsers(){
         return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a User by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the User",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content) })
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable("id") long userId) {
         return userService.getById(userId).map(userDTO -> new ResponseEntity<UserDTO>(userDTO, HttpStatus.OK))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Create a new User")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid body supplied",
+                    content = @Content)})
     @PostMapping()
     public ResponseEntity<UserDTO> saveUser(@RequestBody UserCreationDTO userCreationDTO){
         return new ResponseEntity<>(userService.save(userCreationDTO), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update a User by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User updated successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid body supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content) })
     @PutMapping("/{id}")
     public ResponseEntity<UserDTO> updateUser(@PathVariable("id") long userId,
                                               @RequestBody UserCreationDTO userCreationDTO) {
@@ -41,6 +77,15 @@ public class UserController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Delete a User by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User deleted successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content) })
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable("id") int userId) {
         if(userService.delete(userId)) {
